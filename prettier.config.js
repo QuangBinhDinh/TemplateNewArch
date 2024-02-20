63c3e2620ccdbf61edd4287be3705989e2d3fed7
QuangBinhDinh <g4.terminator@gmail.com>
2024-02-20 13:38:01 +0700

    Build successful new arch

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  parser: 'babel-ts',
+  semi: true,
+  endOfLine: 'auto',
+  arrowParens: 'avoid',
+  tabWidth: 4,
+  // importOrderParserPlugins: ['typescript'],
+};
